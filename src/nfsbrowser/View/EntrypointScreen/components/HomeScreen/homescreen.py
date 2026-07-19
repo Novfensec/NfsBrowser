2bd99d2768f4dee48535cbbf1c877dca026f527e
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-07-19 14:59:52 +0530

    update

--- src/nfsbrowser/View/EntrypointScreen/components/HomeScreen/homescreen.py
+++ src/nfsbrowser/View/EntrypointScreen/components/HomeScreen/homescreen.py
@@ -9,9 +9,6 @@ class FixedTextInput(CTextInput):
         super().__init__(**kwargs)
 
     def window_on_textedit(self, window, ime_input):
-        if self.readonly or self.disabled:
-            return
-
         if not self._lines:
             self._refresh_text(self.text)
 
@@ -19,47 +16,46 @@ class FixedTextInput(CTextInput):
             self.delete_selection()
 
         current_ci = self.cursor_index()
+        cursor_restored = False
 
         if self._ime_composition:
-            ci = getattr(self, '_ime_absolute_cursor', None)
-            if ci is None:
-                ci = self.cursor_index(self._ime_cursor)
+            ci = getattr(
+                self, "_ime_absolute_cursor", self.cursor_index(self._ime_cursor)
+            )
             start_ci = max(0, ci - len(self._ime_composition))
 
-            removed = False
             if self.text[start_ci:ci] == self._ime_composition:
-                current_ci = self._remove_ime_range(start_ci, ci, current_ci)
-                removed = True
-            else:
-                start_ci2 = max(0, current_ci - len(self._ime_composition))
-                if self.text[start_ci2:current_ci] == self._ime_composition:
-                    current_ci = self._remove_ime_range(
-                        start_ci2, current_ci, current_ci)
-                    removed = True
+                self._selection_from = start_ci
+                self._selection_to = ci
+                self._selection = True
+                self.delete_selection()
 
-            if not removed:
-                pass
+                if current_ci >= ci:
+                    current_ci -= len(self._ime_composition)
+                elif current_ci > start_ci:
+                    current_ci = start_ci
+                cursor_restored = True
+
+            else:
+                start_ci_curr = max(0, current_ci - len(self._ime_composition))
+                if self.text[start_ci_curr:current_ci] == self._ime_composition:
+                    self._selection_from = start_ci_curr
+                    self._selection_to = current_ci
+                    self._selection = True
+                    self.delete_selection()
+
+                    current_ci = start_ci_curr
+                    cursor_restored = True
+
+        if cursor_restored:
+            self.cursor = self.get_cursor_from_index(current_ci)
 
         if ime_input:
-            self.insert_text(ime_input, from_undo=True)
+            self.insert_text(ime_input)
 
         self._ime_composition = ime_input
         self._ime_cursor = self.cursor
         self._ime_absolute_cursor = self.cursor_index()
-
-    def _remove_ime_range(self, start_ci, end_ci, current_ci):
-        self._selection_from = start_ci
-        self._selection_to = end_ci
-        self._selection = True
-        self.delete_selection(from_undo=True)
-
-        if current_ci >= end_ci:
-            current_ci -= (end_ci - start_ci)
-        elif current_ci > start_ci:
-            current_ci = start_ci
-
-        self.cursor = self.get_cursor_from_index(current_ci)
-        return current_ci
 
 
 class HomeScreen(BaseScreenView):
